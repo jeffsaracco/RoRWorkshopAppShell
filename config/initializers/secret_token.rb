9c601039b78109255352ad67a749bc636197bad1
Jeff Saracco <jeffsaracco@gmail.com>
2013-06-21 20:01:06 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoRWorkshopAppShell::Application.config.secret_token = '04bdcb967cb54e0bc86fc2ce6e0d00af7a95d697cb03339273af8e770207a708571cf93a7cdf75e9582d0e9fe7ec6e7ff143a04354d906149a9811f35e1cf928'
